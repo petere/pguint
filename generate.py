7e183644f3bd373189315f59dfb115ff9686f257
Peter Eisentraut <peter@eisentraut.org>
2015-01-27 23:10:33 -0500

    Add multiplication overflow checks

--- generate.py
+++ generate.py
@@ -34,7 +34,7 @@ def c_operator(op):
 
 def type_bits(typ):
     m = re.search(r'(\d+)', typ)
-    return int(m.group(1))
+    return int(m.group(1)) * 8
 
 
 def type_unsigned(typ):
@@ -52,6 +52,12 @@ max_values = {
     'uint8': '18446744073709551615',
 }
 
+
+def next_bigger_type(typ):
+    m = re.match(r'(\w+)(\d+)', typ)
+    return m.group(1) + str(int(m.group(2)) * 2)
+
+
 f_operators_c = open('operators.c', 'w')
 f_operators_sql = open('operators.sql', 'w')
 f_test_operators_sql = open('test/sql/operators.sql', 'w')
@@ -61,7 +67,9 @@ f_operators_c.write('#include <fmgr.h>\n\n')
 f_operators_c.write('#include "uint.h"\n\n')
 
 
-def write_c_function(f, funcname, leftarg, rightarg, op, rettype, c_check=''):
+def write_c_function(f, funcname, leftarg, rightarg, op, rettype, c_check='', intermediate_type=None):
+    if intermediate_type is None:
+        intermediate_type = rettype
     f.write("""
 PG_FUNCTION_INFO_V1(%s);
 Datum
@@ -69,14 +77,17 @@ Datum
 {
 	%s arg1 = PG_GETARG_%s(0);
 	%s arg2 = PG_GETARG_%s(1);
-	%s result = arg1 %s arg2;
+	%s result = %sarg1 %s %sarg2;
 %s
 	PG_RETURN_%s(result);
 }
 """ % (funcname, funcname,
        c_types[leftarg], c_types[leftarg].upper(),
        c_types[rightarg], c_types[rightarg].upper(),
-       c_types[rettype], c_operator(op),
+       c_types[intermediate_type],
+       "(%s)" % c_types[intermediate_type] if intermediate_type else '',
+       c_operator(op),
+       "(%s)" % c_types[intermediate_type] if intermediate_type else '',
        ("""
 	if (%s)
 		ereport(ERROR,
@@ -100,9 +111,9 @@ CREATE OPERATOR %s (
        op, funcname, leftarg, rightarg))
 
 
-def write_code(f_c, f_sql, leftarg, rightarg, op, rettype, c_check=''):
+def write_code(f_c, f_sql, leftarg, rightarg, op, rettype, c_check='', intermediate_type=None):
     funcname = leftarg + rightarg + op_words[op]
-    write_c_function(f_c, funcname, leftarg, rightarg, op, rettype, c_check)
+    write_c_function(f_c, funcname, leftarg, rightarg, op, rettype, c_check, intermediate_type)
     write_sql_operator(f_sql, funcname, leftarg, rightarg, op, rettype)
 
 
@@ -139,12 +150,22 @@ for leftarg in new_types + old_types:
                     c_check = '!SAMESIGN(arg1, arg2) && !SAMESIGN(result, arg1)'
                 else:
                     c_check = ''
-            write_code(f_operators_c, f_operators_sql, leftarg, rightarg, op, rettype, c_check)
+            if op == '*':
+                if type_bits(rettype) < 64:
+                    intermediate_type = next_bigger_type(rettype)
+                    c_check = '(%s) result != result' % c_types[rettype]
+                elif type_unsigned(rettype):
+                    c_check = '(arg1 != ((uint32) arg1) || arg2 != ((uint32) arg2)) && (arg2 != 0 && ((arg2 == -1 && arg1 < 0 && result < 0) || result / arg2 != arg1))'
+                else:
+                    c_check = '(arg1 != ((int32) arg1) || arg2 != ((int32) arg2)) && (arg2 != 0 && ((arg2 == -1 && arg1 < 0 && result < 0) || result / arg2 != arg1))'
+            else:
+                intermediate_type = None
+            write_code(f_operators_c, f_operators_sql, leftarg, rightarg, op, rettype, c_check, intermediate_type)
             f_test_operators_sql.write("SELECT pg_typeof('1'::%s %s '1'::%s);\n" % (leftarg, op, rightarg))
             f_test_operators_sql.write("SELECT '1'::%s %s '1'::%s;\n" % (leftarg, op, rightarg))
             f_test_operators_sql.write("SELECT '3'::%s %s '4'::%s;\n" % (leftarg, op, rightarg))
             f_test_operators_sql.write("SELECT '5'::%s %s '2'::%s;\n" % (leftarg, op, rightarg))
-            if op == '+':
+            if op in ['+', '*']:
                 f_test_operators_sql.write("SELECT '%s'::%s %s '%s'::%s;\n" % (max_values[leftarg], leftarg, op, max_values[rightarg], rightarg))
         f_test_operators_sql.write("\n")
 
