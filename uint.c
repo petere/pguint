304631bf8be4684d7ff043c6665ac37525642808
Peter Eisentraut <peter@eisentraut.org>
2015-02-15 00:42:19 -0500

    Factor out pg_atou

--- uint.c
+++ uint.c
@@ -30,12 +30,11 @@ int1out(PG_FUNCTION_ARGS)
 	PG_RETURN_CSTRING(result);
 }
 
-PG_FUNCTION_INFO_V1(uint1in);
-Datum
-uint1in(PG_FUNCTION_ARGS)
+uint32
+pg_atou(const char *s, int size, int c)
 {
-	char	   *s = PG_GETARG_CSTRING(0);
 	unsigned long int result;
+	bool	out_of_range = false;
 
 	if (s == NULL)
 		elog(ERROR, "NULL pointer");
@@ -53,12 +52,43 @@ uint1in(PG_FUNCTION_ARGS)
 
 	errno = 0;
 	result = strtoul(s, NULL, 10);
-	if (errno == ERANGE || result > UCHAR_MAX)
+	switch (size)
+	{
+		case sizeof(uint32):
+			if (errno == ERANGE
+#if defined(HAVE_LONG_INT_64)
+				|| result > UINT_MAX
+				#endif
+				)
+				out_of_range = true;
+			break;
+		case sizeof(uint16):
+			if (errno == ERANGE || result > USHRT_MAX)
+				out_of_range = true;
+			break;
+		case sizeof(uint8):
+			if (errno == ERANGE || result > UCHAR_MAX)
+				out_of_range = true;
+			break;
+		default:
+			elog(ERROR, "unsupported result size: %d", size);
+	}
+
+	if (out_of_range)
 		ereport(ERROR,
 				(errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
-				 errmsg("value \"%s\" is out of range for type uint1", s)));
+				 errmsg("value \"%s\" is out of range for type uint%d", s, size)));
 
-	PG_RETURN_UINT8(result);
+	return result;
+}
+
+PG_FUNCTION_INFO_V1(uint1in);
+Datum
+uint1in(PG_FUNCTION_ARGS)
+{
+	char	   *s = PG_GETARG_CSTRING(0);
+
+	PG_RETURN_UINT8(pg_atou(s, sizeof(uint8), '\0'));
 }
 
 PG_FUNCTION_INFO_V1(uint1out);
@@ -77,30 +107,8 @@ Datum
 uint2in(PG_FUNCTION_ARGS)
 {
 	char	   *s = PG_GETARG_CSTRING(0);
-	unsigned long int result;
 
-	if (s == NULL)
-		elog(ERROR, "NULL pointer");
-	if (*s == 0)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-				 errmsg("invalid input syntax for unsigned integer: \"%s\"",
-						s)));
-
-	if (strchr(s, '-'))
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-				 errmsg("invalid input syntax for unsigned integer: \"%s\"",
-						s)));
-
-	errno = 0;
-	result = strtoul(s, NULL, 10);
-	if (errno == ERANGE || result > USHRT_MAX)
-		ereport(ERROR,
-				(errcode(ERRCODE_NUMERIC_VALUE_OUT_OF_RANGE),
-				 errmsg("value \"%s\" is out of range for type uint2", s)));
-
-	PG_RETURN_UINT16(result);
+	PG_RETURN_UINT16(pg_atou(s, sizeof(uint16), '\0'));
 }
 
 PG_FUNCTION_INFO_V1(uint2out);
@@ -119,25 +127,8 @@ Datum
 uint4in(PG_FUNCTION_ARGS)
 {
 	char	   *s = PG_GETARG_CSTRING(0);
-	unsigned long int result;
 
-	if (s == NULL)
-		elog(ERROR, "NULL pointer");
-	if (*s == 0)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-				 errmsg("invalid input syntax for unsigned integer: \"%s\"",
-						s)));
-
-	if (strchr(s, '-'))
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-				 errmsg("invalid input syntax for unsigned integer: \"%s\"",
-						s)));
-
-	result = strtoul(s, NULL, 10);
-
-	PG_RETURN_UINT32(result);
+	PG_RETURN_UINT32(pg_atou(s, sizeof(uint32), '\0'));
 }
 
 PG_FUNCTION_INFO_V1(uint4out);
